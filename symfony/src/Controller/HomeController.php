fcb0586032cfe2425c00c31d79cf0195d6f84345
deswelle-j <deswelle.johan@gmail.com>
2021-04-29 18:16:05 +0200

    new archi with docker

---
+++ symfony/src/Controller/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/home", name="home")
+     *@method({"GET"})
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/HomeController.php',
+        ]);
+    }
+
+}
